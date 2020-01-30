6ffa57cd74a422e7233af65912706db60081ef55
IdleScV <weschen1996@gmail.com>
2020-01-29 23:59:01 -0500

    created more People methods.
    Added spec file for task management.
    Added test.rb for disposable testing.

--- people.rb
+++ people.rb
@@ -1,19 +1,79 @@
-#! = TO DO
-#! Jobs... feeder, cleaner,cooks, owner
-#!feeders feed animals, make them hungry by -1
-#!cleaners clean,must clean once a day. makes them hungry by -1
-#!cooks cook, making themselves hungry by -1, but gives everyone +2 in hunger
-#!owner, calls the end of the day. hunger -1 once a day.
-
-
+require 'pry'
 class People
-    attr_accessor :job, 
-    attr_reader :name, :hunger
+    #//                                     
+    #! Below is presets
 
-    def initialize(name, job)
+    attr_accessor :job
+    attr_reader :name
+
+    @@all = []
+    @@jobs = {:owner => [], :farmhands => [], :farmers => [], :cooks =>[], :nojob => []}
+
+    #! Above is presets
+    #//                                     
+    #! Below is all for adding new people
+
+    def sortjob(jobtype)
+        # binding.pry
+        case jobtype
+            # binding.pry
+            when 'cooks'
+                then @@jobs[:cooks].push self
+            when 'farmhands'
+                then @@jobs[:farmhands].push self
+            when 'farmers'
+                then @@jobs[:farmers].push self
+            when 'owner'
+                then @@jobs[:owner].push self
+            when 'nojob'
+                then @@jobs[:nojob].push self
+        end
+    end
+    
+    def initialize(name, job='nojob')
         @name = name
         @job = job
-        @hunger = 3
+        @hunger = 5
+        @@all << self
+        sortjob(@job)
+    end
+
+    #! Above is all for adding new people
+    #//                                     
+    #! Below is all for multiple persons
+
+    def self.all
+        @@all
+    end
+
+    def self.jobs(type='all')
+        case type
+            when 'all'
+                @@jobs.map{|k, v| v.map{|person| "#{person.job}: #{person.name} at #{person.hunger} hunger level."}}
+            when 'cooks'
+                @@jobs[:cooks].map{|person| "#{person.name} at #{person.hunger} hunger level."}
+            when 'farmhands'
+                @@jobs[:farmhands].map{|person| "#{person.name} at #{person.hunger} hunger level."}
+            when 'farmers'
+                @@jobs[:farmhands].map{|person| "#{person.name} at #{person.hunger} hunger level."}
+            when 'owner'
+                @@jobs[:owner].map{|person| "#{person.name} at #{person.hunger} hunger level."}
+            when 'unemployeed'
+                @@jobs[:nojob].map{|person| "#{person.name} at #{person.hunger} hunger level."}
+            end
+    end
+
+    #! Above is all for multiple persons
+    #//                                         
+    #! Bellow is all for one person
+
+    #! INFO
+    def person_info
+        "#{@name} is a #{@job} and their hunger is at #{@hunger}"
+    end
+    #! HUNGER
+    def hunger
+        @hunger > 0 ? "#{@name} is at #{@hunger} hunger level." : "#{@name} needs to eat"
     end
 
     def hunger=(hunger)
@@ -25,12 +85,64 @@ class People
             @hunger = hunger
         end
     end
+    #? eat increase 4 hunger
+    def eat
+        self.hunger(self.hunger + 2)
+    end
 
+    #! WORK
+    def can_work?
+        @hunger > 0
+    end
 
-
-
-
-
+    def work
+        self.can_work? ? self.hunger=(@hunger - 1) : "Too hungry to work"
+    end
 
 
 end
+
+#! List of test people
+owner = People.new('Alessio Curran', 'owner')
+cook1 = People.new('Ricardo Salgado', 'cooks')
+cook2 = People.new('Lois Nixon', 'cooks')
+farmhand1 = People.new('Anaya Mcmanus', 'farmhands')
+farmhand2 = People.new('Rohit Barrera', 'farmhands')
+farmhand3 = People.new('Eesha Greenaway', 'farmhands')
+farmer1 = People.new('Jena Humphreys', 'farmers')
+farmer2 = People.new('Demi-Lee Rollins', 'farmers')
+nojob1 = People.new('Nathan Lewis')
+
+
+#! test functioning
+#? multiple people info test
+# puts People.all
+# puts People.jobs
+# p People.jobs('owner')
+# p People.jobs('cooks')
+# p People.jobs('farmhands')
+
+#? singular info test
+# p owner.job
+# p owner.person_info
+
+#? Hunger + work test
+p cook1.hunger
+p cook1.work
+p cook1.hunger
+p cook1.work
+p cook1.hunger
+p cook1.work
+p cook1.hunger
+p cook1.work
+p cook1.hunger
+p cook1.work
+p cook1.hunger
+p cook1.work
+p cook1.hunger
+
+
+#? in Farm, make a method of newPerson(name, job title) that calls People.new(job ,title)
+
+
+
---
+++ specs.rb
@@ -0,0 +1,27 @@
+#! People class
+#* (DONE) .all returns all people 
+#* (DONE) .jobs(type) returns a hash of job types each with an array of people variables. Optional variable of job type to return speicifc array. 
+#* (DONE) returns a person's relative #hunger and #name 
+#* (Busy work. need CASE + add job description)return a person's #job with job description
+#* (DONE) #person_info puts a person's job, hunger, and name 
+
+
+#* (DONE)#can_work? checks if the person can do their job 
+#* (DONE)#hunger= limits hunger to a range of (0...5)
+
+#* (DONE)#work works a person, decreasing their hunger by 1
+#* #work has a switch depending on the person's job type
+    #* cooks can turn animal products into food
+    #? can work twice a day
+        #* chickenthigh = 10 food
+        #* milk = 5 food
+    #* farmhands can feed all animals and get produce
+    #* farmers can get produce from an animal
+    #* owners can call the day off. never goes hungry. 
+
+#* #eat(foodamount) increase a person's hunger by the amount of food
+
+
+
+
+#? Things that are ? are things to implement in the future
